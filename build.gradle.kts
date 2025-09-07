ec3c1ff755fc48a60e81e87dff0fa97843fe35fc
janiomoscote <95260369+janiomoscote@users.noreply.github.com>
2025-09-07 18:36:42 -0500

    Subir cambios de hoy 07-SEP-2025

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.11.1" apply false // Or some other version
-    id("com.android.library") version "8.11.1" apply false    // Or some other version
+    id("com.android.application") version "8.13.0" apply false // Or some other version
+    id("com.android.library") version "8.13.0" apply false    // Or some other version
     // ... other plugins
 }
